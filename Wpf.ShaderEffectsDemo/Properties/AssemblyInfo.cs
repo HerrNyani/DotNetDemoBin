7e556b159d1eef3fd14bf3ea4c007ea2d1893b4a
HerrNyani <jan.deplorez@gmail.com>
2019-04-13 11:57:31 +0200

    Updated assembly info.

--- Wpf.ShaderEffectsDemo/Properties/AssemblyInfo.cs
+++ Wpf.ShaderEffectsDemo/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wpf.ShaderEffectsDemo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ShaderEffectsDemo")]
+[assembly: AssemblyDescription("This demo showcases the use of a ShaderEffect subclass to access a compiled PixelShader, which enables access to GPU-computed effect in WPF.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HerrNyani")]
 [assembly: AssemblyProduct("Wpf.ShaderEffectsDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © HerrNyani/J.R. Deplorez 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
