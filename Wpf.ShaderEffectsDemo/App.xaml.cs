28e85f399b2de56ff175b73b4d3736b08bd3a8a1
HerrNyani <jan.deplorez@gmail.com>
2019-04-13 09:37:33 +0200

    Added ShaderEffectsDemo project.

---
+++ Wpf.ShaderEffectsDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace DotNetDemoBin.Wpf.ShaderEffectsDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
