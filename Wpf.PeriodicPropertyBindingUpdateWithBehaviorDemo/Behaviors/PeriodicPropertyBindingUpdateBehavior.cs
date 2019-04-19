acd01cbf8109068647a37bbaa795692bad0504ba
HerrNyani <jan.deplorez@gmail.com>
2019-04-19 13:40:38 +0200

    Added PeriodicPropertyBindingUpdateBehavior class.

---
+++ Wpf.PeriodicPropertyBindingUpdateWithBehaviorDemo/Behaviors/PeriodicPropertyBindingUpdateBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+using System.Windows.Threading;
+
+namespace DotNetDemoBin.Wpf.PeriodicPropertyBindingUpdateWithBehaviorDemo.Behaviors
+{
+    public class PeriodicPropertyBindingUpdateBehavior :
+        Behavior<DependencyObject>
+    {
+        private readonly DispatcherTimer _updatePropertyBindingTimer = new DispatcherTimer(DispatcherPriority.DataBind);
+
+        public PeriodicPropertyBindingUpdateBehavior()
+        {
+            _updatePropertyBindingTimer.Tick += UpdatePropertyBindingTimer_HandleTick;
+        }
+
+        public TimeSpan Interval { get; set; } = TimeSpan.FromSeconds(1);
+        public DependencyProperty PropertyToUpdate { get; set; }
+
+        protected override void OnAttached()
+        {
+            if(PropertyToUpdate == null)
+            {
+                throw new InvalidOperationException($"Property {PropertyToUpdate} must be set.");
+            }
+
+            _updatePropertyBindingTimer.Interval = Interval;
+            _updatePropertyBindingTimer.Start();
+
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            _updatePropertyBindingTimer.Stop();
+
+            base.OnDetaching();
+        }
+
+        private void UpdatePropertyBindingTimer_HandleTick(object sender, EventArgs e)
+        {
+            BindingOperations.GetBindingExpression(AssociatedObject, PropertyToUpdate)?.UpdateTarget();
+        }
+    }
+}
