52c9bd95f2524fc78ca28ae7e08a1bf17b8eb32b
HerrNyani <jan.deplorez@gmail.com>
2019-04-19 13:14:00 +0200

    Added periodic property binding update demo project.

---
+++ Wpf.PeriodicPropertyBindingUpdateWithBehaviorDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace DotNetDemoBin.Wpf.PeriodicPropertyBindingUpdateWithBehaviorDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
