ad5586f7adbfc700154f362046762e0071ca9e1b
HerrNyani <jan.deplorez@gmail.com>
2019-05-08 18:26:34 +0200

    Added load and display logic for configuration file contents.

--- DotNetFramework.CustomConfigurationSectionsDemo/Program.cs
+++ DotNetFramework.CustomConfigurationSectionsDemo/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DotNetDemoBin.DotNetFramework.CustomConfigurationSectionsDemo.ApplicationConfiguration.Chat;
+using System;
+using System.Configuration;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNetDemoBin.DotNetFramework.CustomConfigurationSectionsDemo
 {
@@ -10,6 +9,33 @@ namespace DotNetDemoBin.DotNetFramework.CustomConfigurationSectionsDemo
     {
         static void Main(string[] args)
         {
+            var chatConfig = ConfigurationManager.GetSection("chat") as ChatConfigurationSection;
+
+            if (chatConfig == null)
+            {
+                Console.WriteLine("No chat section found in app.config.");
+                return;
+            }
+
+            Console.WriteLine("=== Loaded configuration ===");
+            Console.WriteLine("--- Text ---");
+            Console.WriteLine($"Text color: {chatConfig.Text.TextColorName}");
+            Console.WriteLine();
+
+            Console.WriteLine("--- Voice ---");
+            Console.WriteLine($"Activation mode: {chatConfig.Voice.ActivationModeName}");
+            Console.WriteLine($"\tPush to talk key: {chatConfig.Voice.PushToTalkActivation.ActivationKeyName}");
+            Console.WriteLine($"\tVoice activation threshold percentage: {chatConfig.Voice.VoiceActivation.VolumeThresholdPercentage}");
+            Console.WriteLine();
+
+            Console.WriteLine("--- Servers ---");
+            foreach (var serverConfig in chatConfig.ChatServerConfigurationElementCollection
+                .OfType<ChatServerConfigurationElement>())
+            {
+                Console.WriteLine($"\t{serverConfig.DisplayName} ({serverConfig.Address})");
+            }
+
+            Console.WriteLine();
         }
     }
 }
