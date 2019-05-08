99f431e43020790bc80d518bb850d39f8e82e13a
HerrNyani <jan.deplorez@gmail.com>
2019-05-08 18:10:31 +0200

    Added CustomConfigurationSectionsDemo.

---
+++ DotNetFramework.CustomConfigurationSectionsDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShaderEffectsDemo")]
+[assembly: AssemblyDescription("This demo showcases the use of a ShaderEffect subclass to access a compiled PixelShader, which enables access to GPU-computed effect in WPF.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HerrNyani")]
+[assembly: AssemblyProduct("Wpf.ShaderEffectsDemo")]
+[assembly: AssemblyCopyright("Copyright © HerrNyani/J.R. Deplorez 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fa44caa7-5523-41d4-8fa2-f1fdb1a90ec8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
